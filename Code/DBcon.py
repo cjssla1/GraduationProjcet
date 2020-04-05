04d6256bc3b58595c7a6e0163711df40f7e53e87
cjssla1 <89kkl@naver.com>
2020-04-05 17:18:08 +0900

    버전확인 추가

--- Code/DBcon.py
+++ Code/DBcon.py
@@ -1,20 +1,22 @@
-import MySQLdb
+#!usr/bin/python3
 
-conn = MySQLdb.connect(host='10.100.100.4', port=3306, user='dbid', passwd='pw', db='dbname', charset='utf8')
-#import mysql.connector
-#conn = mysql.connector.connect(host='10.100.100.4', port='3306', user='dbid', password='pw', database='dbname', charset='utf8')
+db = MySQLdb.connect("localhost","admin","1234","stock")
 
+cursor = db.cursor()
 
+cursor.execute("SELECT VERSION()")
 
-curs = conn.cursor()
+data = cursor.fetchone()
+
+print("Database version : {}".format(data))
 
 
 sql = "select * from table_A"
-curs.execute(sql)
+cursor.execute(sql)
 
-data = curs.fetchall()
-data[0] #첫번째 row
-data[1] #두번째 row
+data = cursor.fetchall()
+#data[0] #첫번째 row
+#data[1] #두번째 row
 
 
 conn.close()
